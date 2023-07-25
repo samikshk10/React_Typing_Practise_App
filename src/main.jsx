5c481a3b8d74194f0a5a448c842ef554dae51c0d
samik shk <samik.shakya8848@gmail.com>
2023-07-25 18:00:21 +0545

    character and word completed

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Character from "./components/character";
+import Word from "./components/Words";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Character />,
+  },
+  {
+    path: "/signup",
+    element: <Word />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
